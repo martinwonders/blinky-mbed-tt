20ec583ea4fef09b8bd05b41dc91775ab3a67538
martinwonders <martin.wonders@unn.ac.uk>
2019-10-21 03:04:06 -0700

    Exercise 2 tested

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -13,29 +13,44 @@
 #include <mbed.h>
 
 /* Declare function prototypes */
+void led1ToggleTask(void);
+void led2ToggleTask(void);
 
 /* Declare varaibles for the LEDs etc */
+DigitalOut led1(LED1);
+DigitalOut rled_mbed(D5);
+DigitalOut gled_mbed(D9);
 
 int main () {
   /* Initialise the LEDs */
+  led1 = 1;
+  rled_mbed = 1;
+  gled_mbed = 1;
 
   /* Initialise the scheduler. Ensure TT_SCHED_TICK_HZ gives 1 ms between ticks */
+  schInit();
 
   /* Add tasks to the scheduler table */
+  schAddTask(led1ToggleTask, 0, 500);
+  schAddTask(led2ToggleTask, 500, 500);
 
   /* Start the scheduler */
+  schStart();
   
   while (true) {
     /* Main application loop -- dispatch tasks */
+    schDispatch();
   }
 }
 
 
 void led1ToggleTask(void) {
   /* Toggle the red LED on the K64F board */
+  led1 = !led1;
 }
 
 void led2ToggleTask(void) {
   /* Toggle the green LED on the application board */
+  gled_mbed = !gled_mbed;
 }
 
